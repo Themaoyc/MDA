817f2759b0b1d2b4681f7279dbb4e34056c22b01
Themaoyc <109041393+Themaoyc@users.noreply.github.com>
2022-08-07 03:08:55 +0200

    11

--- Data Visualization&APP/main.py
+++ Data Visualization&APP/main.py
@@ -1,29 +1,32 @@
-from dash import Dash, html, dcc, Input, Output, dash_table,callback
+from dash import Dash, html, dcc, Input, Output, dash_table, callback
 from dash.exceptions import PreventUpdate
 import pandas as pd
 import dash
 import dash_bootstrap_components as dbc
-from dash import html,dcc
+from dash import html, dcc
 import plotly.graph_objects as go
 import plotly.express as px
 from dash.dependencies import Input, Output
+import matplotlib.pyplot as plt
 import json
-app = Dash(__name__,suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.ZEPHYR])
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.ZEPHYR])
 df1 = pd.read_csv('https://github.com/Themaoyc/MDA/blob/main/Data/temperaturedata_predict.csv?raw=true')
+df1long = df1.melt(id_vars=['Country', 'Year','Heatwave'],
+                   value_vars=['January', 'February','March','April','May'],
+                   var_name='Month',
+                   value_name='tavg')
 df2 = pd.read_csv('https://github.com/Themaoyc/MDA/blob/main/Data/deaths_predict.csv?raw=true')
-df2.drop(['ISO','CPI'], axis=1,inplace=True)
-df2long = df2.melt(id_vars = ['Country', 'Year'],
-                    value_vars = ['tmax','duration','GDP(million dollars)','Population','healthexp','Associated Drought',
-                   'Associated Wildfire','Appeal or Declaration','Total Deaths'],
-                    var_name = 'Indicator Name',
-                    value_name = 'Value')
-df3 = pd.read_csv('https://github.com/Themaoyc/MDA/blob/main/Data/Latitude%26Longitude.csv?raw=true')
-df4 = pd.read_csv('https://github.com/Themaoyc/MDA/blob/main/Data/emdat%20heatwave.csv?raw=true')
-df3 = df3.rename(columns={'Alpha-3 code':'ISO'})
-df4 = df4[['ISO','Year']]
-df5 = pd.merge(df4,df3,on=['ISO'],how='left')
-df5 = df5[['Country','Year','Latitude (average)','Longitude (average)']]
+df2.drop(['ISO', 'CPI'], axis=1, inplace=True)
+df2long = df2.melt(id_vars=['Country', 'Year'],
+                   value_vars=['tmax', 'duration', 'GDP(million dollars)', 'Population', 'healthexp',
+                               'Associated Drought',
+                               'Associated Wildfire', 'Appeal or Declaration', 'Total Deaths'],
+                   var_name='Indicator Name',
+                   value_name='Value')
 
+df4 = pd.read_csv('https://github.com/Themaoyc/MDA/blob/main/Data/emdat%20heatwave.csv?raw=true')
+
+df4 = df4[['ISO','Year','Disaster Subtype']]
 
 
 tabs_styles = {
@@ -43,99 +46,120 @@ tab_selected_style = {
     'padding': '6px'
 }
 
-
-
 app.layout = html.Div([
-    dcc.Tabs(id="tabs-inline", value='tab-1', parent_className='custom-tabs',className='custom-tabs-container',
+    dcc.Tabs(id="tabs-inline", value='tab-1', parent_className='custom-tabs', className='custom-tabs-container',
              children=[
-        dcc.Tab(label='Homepage', value='Homepage', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Predict Heatwaves', value='Predict Heatwaves', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Predict Deaths', value='Predict Deaths', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Tab 4', value='tab-4', style=tab_style, selected_style=tab_selected_style),
-    ], style=tabs_styles),
+                 dcc.Tab(label='Homepage', value='Homepage', style=tab_style, selected_style=tab_selected_style),
+                 dcc.Tab(label='Predict Heatwaves', value='Predict Heatwaves', style=tab_style,
+                         selected_style=tab_selected_style),
+                 dcc.Tab(label='Predict Deaths', value='Predict Deaths', style=tab_style,
+                         selected_style=tab_selected_style),
+                 dcc.Tab(label='Tab 4', value='tab-4', style=tab_style, selected_style=tab_selected_style),
+             ], style=tabs_styles),
     html.Div(id='tabs-content')
 ])
+
 
 @app.callback(Output('tabs-content', 'children'),
               Input('tabs-inline', 'value'))
 def render_content(tab):
     if tab == 'Homepage':
         return html.Div([
-            html.H1('MDA Project Heatwave',style={'textAlign': 'center'}),
+            html.H1('MDA Project Heatwave', style={'textAlign': 'center'}),
+            html.H3('These years, public are paying more and more attention to heat waves as such '
+                    'extreme meteorological may could lead to massive deaths . '
+                    'In order to have a better knowledge on the damage due to heat waves, '
+                    'our group decide to predict the heatwave and deaths caused by heatwaves.'),
+
             html.Div([
-                dcc.Graph(id='World Heatwave'),
+                dcc.Graph(id='World Heatwave',figure=px.choropleth(df4, locations="ISO", color="Disaster Subtype",  animation_frame="Year",
+                                                                   )),
 
-                dcc.Slider(
-                    df5['Year'].min(),
-                    df5['Year'].max(),
-                    step=None,
-                    id='year--slider1',
-                    value=df5['Year'].max(),
-                    marks={str(year): str(year) for year in df5['Year'].unique()},
-
-                )], style={'width': '48%', 'textAlign': 'center', 'float': 'center', 'display': 'inline',
-                           'margin-left': '-300px', 'margin-right': '-300px'})
+                ], style={'width': '60%', 'textAlign': 'center', 'float': 'center', 'display': 'inline',
+                           'margin-left': '-300px', 'margin-right': '-300px'}),
+            html.H6('Worldwide Heatwave Map', style={'textAlign': 'center'})
         ])
     elif tab == 'Predict Heatwaves':
         return html.Div([
-            html.H3('Tab content 2')
+            html.H1('Predict Heatwaves', style={'textAlign': 'center'}),
+            html.H3('In this part, we try to use the monthly average temperature from January to February'
+                    'to predict whether there will be a heatwave during the summer. '),
+            html.Div([
+                dcc.Graph(id='AVGt'),
+
+                dcc.Slider(
+                    df1long['Year'].min(),
+                    df1long['Year'].max(),
+                    step=None,
+                    id='year--slider3',
+                    value=df1long['Year'].max(),
+                    marks={str(year): str(year) for year in df1long['Year'].unique()},
+
+                )], style={'width': '48%', 'textAlign': 'center', 'float': 'center', 'display': 'inline',
+                           'margin-left': '-300px', 'margin-right': '-300px'}),
+            html.H3('We can see that the group with a heatwave in the future has a different trend line'),
+
         ])
+
     elif tab == 'Predict Deaths':
-        return  html.Div([
-    html.Div([
-        html.H3('Predict deaths',style={'textAlign': 'center'}),
-        html.H4('In this part, we try to use GDP, health expenditure, population, max temperature during the heatwave,'
-                'duration of the heatwave,associated disaster, and the declaration in advance to predict the total deaths'
-                'of a heatwave. The correlations of these variables are shown as follows: '),
-        html.Div([
-            html.H5('Xaxis'),
-            dcc.Dropdown(
-                df2long['Indicator Name'].unique(),
-                'tmax',
-                id='xaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='xaxis-type',
-                inline=True
-            )
-        ], style={'width': '48%', 'display': 'inline-block'}),
+        return html.Div([
+            html.Div([
+                html.H1('Predict deaths', style={'textAlign': 'center'}),
+                html.H3(
+                    'In this part, we try to use GDP, health expenditure, population, max temperature during the heatwave,'
+                    'duration of the heatwave,associated disaster, and the declaration in advance to predict the total deaths'
+                    'of a heatwave. The correlations of these variables are shown as follows: '),
+                html.Div([
+                    html.H5('Xaxis'),
+                    dcc.Dropdown(
+                        df2long['Indicator Name'].unique(),
+                        'tmax',
+                        id='xaxis-column'
+                    ),
+                    dcc.RadioItems(
+                        ['Linear', 'Log'],
+                        'Linear',
+                        id='xaxis-type',
+                        inline=True
+                    )
+                ], style={'width': '48%', 'display': 'inline-block'}),
 
-        html.Div([
-            html.H5('Yaxis'),
-            dcc.Dropdown(
-                df2long['Indicator Name'].unique(),
-                'Total Deaths',
-                id='yaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='yaxis-type',
-                inline=True
-            )
-        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
-    ]),
-        html.Div([
-    dcc.Graph(id='Predict deaths1'),
+                html.Div([
+                    html.H5('Yaxis'),
+                    dcc.Dropdown(
+                        df2long['Indicator Name'].unique(),
+                        'Total Deaths',
+                        id='yaxis-column'
+                    ),
+                    dcc.RadioItems(
+                        ['Linear', 'Log'],
+                        'Linear',
+                        id='yaxis-type',
+                        inline=True
+                    )
+                ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+            ]),
+            html.Div([
+                dcc.Graph(id='Predict deaths1'),
 
-    dcc.Slider(
-        df2long['Year'].min(),
-        df2long['Year'].max(),
-        step=None,
-        id='year--slider2',
-        value=df2long['Year'].max(),
-        marks={str(year): str(year) for year in df2long['Year'].unique()},
+                dcc.Slider(
+                    df2long['Year'].min(),
+                    df2long['Year'].max(),
+                    step=None,
+                    id='year--slider2',
+                    value=df2long['Year'].max(),
+                    marks={str(year): str(year) for year in df2long['Year'].unique()},
 
-    )],style={'width': '48%', 'textAlign': 'center','float': 'center', 'display': 'inline','margin-left':'-300px','margin-right':'-300px'})
+                )], style={'width': '48%', 'textAlign': 'center', 'float': 'center', 'display': 'inline',
+                           'margin-left': '-300px', 'margin-right': '-300px'})
 
-])
+        ])
 
     elif tab == 'tab-4':
         return html.Div([
             html.H3('Tab content 4')
         ])
+
 
 @app.callback(
     Output('Predict deaths1', 'figure'),
@@ -150,45 +174,38 @@ def update_graph(xaxis_column_name, yaxis_column_name,
     df2longf = df2long[df2long['Year'] == year_value2]
 
     fig1 = px.scatter(x=df2longf[df2longf['Indicator Name'] == xaxis_column_name]['Value'],
-                     y=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Value'],
-                     hover_name=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Country'],
-                     color=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Country'],
-                    )
+                      y=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Value'],
+                      hover_name=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Country'],
+                      color=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Country'],
+                      size=df2longf[df2longf['Indicator Name'] == yaxis_column_name]['Value']
+                      )
 
     fig1.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
 
     fig1.update_xaxes(title=xaxis_column_name,
-                     type='linear' if xaxis_type == 'Linear' else 'log')
+                      type='linear' if xaxis_type == 'Linear' else 'log')
 
     fig1.update_yaxes(title=yaxis_column_name,
-                     type='linear' if yaxis_type == 'Linear' else 'log')
+                      type='linear' if yaxis_type == 'Linear' else 'log')
 
     return fig1
+
+
+
 @app.callback(
-    Output('World Heatwave', 'figure'),
-    Input('year--slider1', 'value'))
-def update_graph(year_value1):
-    df5f = df5[df5['Year'] == year_value1]
-
-    fig2 = go.Figure(go.Scattermapbox(
-                                      mode='markers',
-                                      lon=df5f[2],
-                                      lat=df5f[3],
-                                      hovertext='Country',
-                                      hoverinfo='text',
-                                      marker_symbol='marker',
-                                      marker_size=10
-                                      ))
-
-    fig2.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+    Output('AVGt', 'figure'),
+    Input('year--slider3', 'value'))
+def update_graph(year_value3):
+    df1longf = df1long[df1long['Year'] == year_value3]
+    fig3 = px.scatter(df1longf, x="Month", y="tavg", color="Heatwave")
 
 
-    return fig2
+    fig3.update_layout(margin={"r": 20, "t": 0, "l": 20, "b": 10})
+
+
+    return fig3
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
 
 
-
-#fig2 = px.scatter_mapbox(df5, lat="Latitude (average)", lon="Longitude (average)",     color="peak_hour", size="car_hours",
-                  #color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
